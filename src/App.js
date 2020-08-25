28ccf2ddcf44c650de24e6f8921f1edd3eeb27a4
Redvan <redvanisation@gmail.com>
2020-08-25 17:46:32 -0400

    Add a fetch function to get data from the API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  const makeRequest = () => {
+    fetch('https://pic-to-code-server.herokuapp.com/')
+    // fetch('http://localhost:5000')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={makeRequest}>
+      Click to API
+    </button>
   );
 }
 
